832d62860b6c734c35c72584f906f3eb1746fa46
Lycoris <windfantasy@wind-fantasydeMacBook-Pro.local>
2019-03-11 13:28:37 +0800

    feat(add): test

--- test-env/app.js
+++ test-env/app.js
@@ -1,15 +1,30 @@
-import React, {useState, userEffect} from '@packages/react';
+import React, {useState, useEffect} from '@packages/react';
 import ReactDOM from '@packages/react-dom';
 
+const init = {count: 0}
+
 function Hooks() {
-  const [count, setCount] = useState(0)
+  debugger;
+  const count = useTestHook(init)
 
   return (<div>
-      <p>you have clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <p>you have clicked {count.count} times</p>
+      {/* <button onClick={() => setCount({count: count+1})}> */}
+      <button>
         Click me
       </button>
     </div>)
+}
+
+function useTestHook() {
+
+  const  [count, setCount] = useState(init)
+
+  // 一起使用会触发无限的循环
+  useEffect(() => {
+    setTimeout(() => setCount({count: count.count+5}), 3000)
+  })
+  return count
 }
 
 ReactDOM.render(<Hooks />, document.getElementById('root'));
