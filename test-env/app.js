1bba64b1469c4a6821465e52daf04007fee88eb7
Lycoris <windfantasy@wind-fantasydeMacBook-Pro.local>
2019-10-08 17:19:26 +0800

    chore(config): learn react

--- test-env/app.js
+++ test-env/app.js
@@ -1,20 +1,10 @@
-import React, {useState, useEffect} from '@packages/react';
+import React, {useState,} from '@packages/react';
 import ReactDOM from '@packages/react-dom';
 
 const init = {count: 0}
 
 function Hooks() {
-  // const count = useTestHook(init)
-
-  // return (<div>
-  //     <p>you have clicked {count.count} times</p>
-  //     {/* <button onClick={() => setCount({count: count+1})}> */}
-  //     <button>
-  //       Click me
-  //     </button>
-  //   </div>)
   const [count, setCount] = useState(0);
-  const [name, setName] = useState('Star');
 
   // 调用三次setCount便于查看更新队列的情况
   const countPlusThree = () => {
@@ -24,21 +14,10 @@ function Hooks() {
   }
   return (
     <div className='App'>
-      <p>{name} Has Clicked <strong>{count}</strong> Times</p>
+      <p> You Have Clicked <strong>{count}</strong> Times</p>
       <button onClick={countPlusThree}>Click *3</button>
     </div>
   )
-}
-
-function useTestHook() {
-
-  const  [count, setCount] = useState(init)
-
-  // 一起使用会触发无限的循环
-  useEffect(() => {
-    setTimeout(() => setCount({count: count.count+5}), 3000)
-  })
-  return count
 }
 
 ReactDOM.render(<Hooks />, document.getElementById('root'));
